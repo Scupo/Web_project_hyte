b5361b432c00c8b41ba726d50e42f16824c2c937
Hansu69 <48755885+Hansu69@users.noreply.github.com>
2019-03-31 00:34:58 +0200

    Add files via upload

--- server.php
+++ server.php
@@ -1,15 +1,21 @@
 <?php
 session_start();
-
 // initializing variables
 $username = "";
 $email    = "";
 $errors = array(); 
-
 // connect to the database
-$config = parse_ini_file("../../config.ini");
-$db = mysqli_connect($config["dbaddr"], $config["username"], $config["password"], $config["dbname"]);
+$servername = "mysql.metropolia.fi";
+$username = "petermoi";
+$salasana = "Hansu69";
+$dbname = "petermoi";
 
+// Create connection
+$db = new mysqli($servername, $username, $salasana, $dbname);
+// Check connection
+if ($db->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
@@ -17,7 +23,6 @@ if (isset($_POST['reg_user'])) {
   $email = mysqli_real_escape_string($db, $_POST['email']);
   $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
-
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
   if (empty($username)) { array_push($errors, "Username is required"); }
@@ -26,10 +31,9 @@ if (isset($_POST['reg_user'])) {
   if ($password_1 != $password_2) {
 	array_push($errors, "The two passwords do not match");
   }
-
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
-  $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+  $user_check_query = "SELECT * FROM Users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
@@ -37,17 +41,14 @@ if (isset($_POST['reg_user'])) {
     if ($user['username'] === $username) {
       array_push($errors, "Username already exists");
     }
-
     if ($user['email'] === $email) {
       array_push($errors, "email already exists");
     }
   }
-
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
-
-  	$query = "INSERT INTO users (username, email, password) 
+  	$query = "INSERT INTO Users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
@@ -55,10 +56,8 @@ if (isset($_POST['reg_user'])) {
   	header('location: home.php');
   }
 }
-
 // ... 
 // LOGIN USER
-
 if (isset($_POST['login_user'])) {
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $password = mysqli_real_escape_string($db, $_POST['password']);
@@ -72,7 +71,7 @@ if (isset($_POST['login_user'])) {
   
     if (count($errors) == 0) {
         $password = md5($password);
-        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $query = "SELECT * FROM Users WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
           $_SESSION['username'] = $username;
