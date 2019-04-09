2b8a0cccce5eeb517f8f7868396f40c5d60b419d
Miika Honni <miika.honni@metropolia.fi>
2019-04-09 14:14:48 +0300

    modified:   server.php

--- server.php
+++ server.php
@@ -5,13 +5,8 @@ $username = "";
 $email    = "";
 $errors = array(); 
 // connect to the database
-$config = parse_ini_file("../../../../config.ini");
+$config = parse_ini_file("../../config.ini");
 $db = mysqli_connect($config["dbaddr"], $config["username"], $config["password"], $config["dbname"]);
-
-// Check connection
-if ($db->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
   // receive all input values from the form
@@ -44,7 +39,7 @@ if (isset($_POST['reg_user'])) {
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
-  	$query = "INSERT INTO Users (username, email, password) 
+  	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
@@ -78,5 +73,3 @@ if (isset($_POST['login_user'])) {
         }
     }
   }
-  
-  ?>
