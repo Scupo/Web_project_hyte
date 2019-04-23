0b4ae90b100f63ab41c2c031d87ad339157afa5b
timoharju <timo.harju@metropolia.fi>
2019-04-23 14:15:16 +0300

    major changes

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
-=======
->>>>>>> 89fee3954bda380e831ecf81cbf081f78fdac9cc
 <?php 
   session_start(); 
 
@@ -20,7 +17,7 @@
 
 
 
-<html lang="en">>
+<html lang="en">
 <head>
 
      <meta charset="UTF-8">
@@ -29,147 +26,69 @@
 	<title>Home</title>
      
      <link rel="stylesheet" type="text/css" href="css/main.css">
+     <link rel="stylesheet" type="text/css" href="css/dropdown.css">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-     <script src="js/clickremove.js" type="text/javascript"></script>  
+     <script src="js/clickremove.js" type="text/javascript"></script>
+     <script src="js/addrow.js" type="text/javascript"></script>
+     <script src="js/dropdown.js" type="text/javascript"></script>      
      <link href="https://fonts.googleapis.com/css?family=Norican" rel="stylesheet"> 
 
+
 </head>
+
+
 <body>
 
-<div class="header">
-	<h2>Home Page</h2>
-</div>
 <div class="content">
-  	<!-- notification message -->
-  	<?php if (isset($_SESSION['success'])) : ?>
-      <div class="error success" >
-      	<h3>
-          <?php 
-               echo $_SESSION['success']; 
-          	unset($_SESSION['success']);
-          ?>
-      	</h3>
-      </div>
-  	<?php endif ?>
-
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
-    	<p>Welcome your username is <strong><?php echo $_SESSION['username'];?></strong> and userid is <strong><?php echo $_SESSION['userid'];?></strong></p>
-    	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+    	
     <?php endif ?>
 </div>
 
-<!--Background picture
-<div class="bgcontainer">
-            <div class="bg-image">
-            </div>-->
-
-<!-- Dynamic Form for adding habits -->
-<div class="container">  
-                <br />  
-                <br />  
-
-                <table class="habit-container">
-	<thead>
-		<tr>
-			<th><h1>Habitname</h1></th>
-			<th><h1>Monday</h1></th>
-			<th><h1>Tuesday</h1></th>
-               <th><h1>Wednesday</h1></th>
-               <th><h1>Thursday</h1></th>
-               <th><h1>Friday</h1></th>
-               <th><h1>Saturday</h1></th>
-               <th><h1>Sunday</h1></th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>Eat breakfast</td>
-			<td></td>
-			<td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-		</tr>
-		<tr>
-			<td>Read a book</td>
-			<td></td>
-			<td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-		</tr>
-		<tr>
-			<td>Meditate</td>
-			<td></td>
-			<td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-
-		</tr>
-    <tr>
-			<td>Yoga</td>
-			<td></td>
-			<td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-		</tr>
-    <tr>
-			<td>Gym</td>
-			<td></td>
-			<td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-		</tr>
-    <tr>
-			<td>Good sleep</td>
-			<td></td>
-			<td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-               <td></td>
-		</tr>
-	</tbody>
-</table>                
-                <h2>Add habits</h2>  
-                <div class="form-group">  
-                     <form name="add_habit" id="add_habit" method="post">  
-                          <div class="table-responsive">  
-                               <table class="table table-bordered" id="dynamic_field">  
-                                    <tr>  
-                                         <td><input type="text" name="habitname[]" placeholder="Habit" class="form-control name_list" /></td>  
-                                         <td><button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>  
-                                    </tr>  
-                               </table>  
-                               <input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" />  
-                          </div>  
-                     </form>  
-                </div>  
-           </div>   
+<div class="container2">  
+<header>
            
-<!--<div class="habit-success">
-        <form method="get" id="HabitSuccess">
-        <input type="checkbox" name="habit"> <br>  
-        <div id="habitadder"></div>
-        <input type="submit" value="Done!" name="checkhabit">
-        </form>
-</div> -->
+           
+<div class="form-group">  
+                <form name="add_habit" id="add_habit" method="post">  
+                    <input type="text" name="habitname" id="habitname" placeholder="Enter your habit" class="form-control name_list">         
+                    <button type="button" onclick="addRow()" onSubmit="return false;" name="submithabit" id="submithabit" class="btn btn-info" value="Submit"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 16 16" style="enable-background:new 0 0 16 16;" xml:space="preserve"><g><path class="fill" d="M16,8c0,0.5-0.5,1-1,1H9v6c0,0.5-0.5,1-1,1s-1-0.5-1-1V9H1C0.5,9,0,8.5,0,8s0.5-1,1-1h6V1c0-0.5,0.5-1,1-1s1,0.5,1,1v6h6C15.5,7,16,7.5,16,8z"/></g></svg></button>  
+                </form> 
+                <div class="dropdown">
+                <button onclick="drop()" class="dropbtn">Menu</button>
+                <div class="divider1"><div>
+                    <div id="myDropdown" class="dropdown-content">
+                        <p id="dropdown-welcome"><strong><?php echo $_SESSION['username'];?></strong></p>
+                        <a href="#">Progress</a>
+                        <a href="#">Achievements</a>
+                        <a href="index.php?logout='1'">Logout</a>
+                    
+                </div>
+            </div>
+            </header>      
+            <div class="habit-table">   
+            <h2>Habits to complete</h2>               
+            <table class ="habit-container">
+               
+            <tr>
+                <th>Habitname</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+            
+            </table>
+</tr>               
+<button type="button">Save</button>
+            
+</div>                
+</div>   
+           
 
 </body> 
 </html>
